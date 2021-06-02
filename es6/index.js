33e880bf86e50bcff24cb73d07580317dba16a48
qian_xiaoan <519637185@qq.com>
2021-06-02 09:21:57 +0800

    es6 扩展

--- es6/index.js
+++ es6/index.js
@@ -122,7 +122,39 @@
 // a.set(tom,1);
 
 /**for of
- * 遍历所有数据结构的同一方式
+ * 遍历所有数据结构的同一方式：因为其内部调用了iterable方法得到一个迭代器，从而去遍历内部所有的数据
  * 1. break 种植循环（foreach不行）；其他可以终止遍历的是some，every
  * 2. 伪数组uekeyi遍历arguments
  */
+
+/**迭代器模式 
+ * 对外提供统一的遍历接口，让外部不用再去关心这个数据内部的结构是怎么样的
+*/
+// const obj = {
+//   name: ['q', 'w', e],
+//   age: [1, 2, 3, 4],
+//   each: function (callback) {
+//     const all = [...this.name, ...this.age]
+//     for (const iterator of all) {
+//       callback(iterator);
+//     }
+//   },
+//   [Symbol.iterator]: function (params) {
+//     const all = [...this.name, ...this.age];
+//     let index = 0;
+//     return {
+//       next: function () {
+//         return {
+//           value: all[index],
+//           done: index++ >= all.length
+          
+//         }
+//        }
+//     }
+//   }
+// }
+
+
+/**生成器
+ * 惰性执行。执行的时候生成了迭代器。通过next()惰性执行
+ */ 
