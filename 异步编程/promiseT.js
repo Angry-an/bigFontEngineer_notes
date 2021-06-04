8c593e8e4d97acc55c0b41ec9b274f51a89e563d
qian_xiaoan <519637185@qq.com>
2021-06-04 15:43:06 +0800

    新增diff优化

--- 异步编程/promiseT.js
+++ 异步编程/promiseT.js
@@ -25,27 +25,30 @@ class PromiseS {
       reject(err);
     }
   }
-  static all (list) {
-  return new PromiseS((resolve, reject) => {
-    /**
-     * 返回值的集合
-     */
-    let values = []
-    let count = 0
-    for (let [i, p] of list.entries()) {
-      // 数组参数如果不是MyPromise实例，先调用MyPromise.resolve
-      this.resolve(p).then(res => {
-        values[i] = res
-        count++
-        // 所有状态都变成fulfilled时返回的MyPromise状态就变成fulfilled
-        if (count === list.length) resolve(values)
-      }, err => {
-        // 有一个被rejected时返回的MyPromise状态就变成rejected
-        reject(err)
-      })
-    }
-  })
-}
+  static all(list) {
+    return new PromiseS((resolve, reject) => {
+      /**
+       * 返回值的集合
+       */
+      let values = [];
+      let count = 0;
+      for (let [i, p] of list.entries()) {
+        // 数组参数如果不是MyPromise实例，先调用MyPromise.resolve
+        this.resolve(p).then(
+          (res) => {
+            values[i] = res;
+            count++;
+            // 所有状态都变成fulfilled时返回的MyPromise状态就变成fulfilled
+            if (count === list.length) resolve(values);
+          },
+          (err) => {
+            // 有一个被rejected时返回的MyPromise状态就变成rejected
+            reject(err);
+          }
+        );
+      }
+    });
+  }
   then(onFulfilled, onRejected) {
     // // onFulfilled如果不是函数，就忽略onFulfilled，直接返回value
     // onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : (value) => value;
@@ -108,6 +111,30 @@ class PromiseS {
     // 返回promise，完成链式
     return promise2;
   }
+  static all(list) {
+    return new MyPromise((resolve, reject) => {
+      /**
+       * 返回值的集合
+       */
+      let values = [];
+      let count = 0;
+      for (let [i, p] of list.entries()) {
+        // 数组参数如果不是MyPromise实例，先调用MyPromise.resolve
+        this.resolve(p).then(
+          (res) => {
+            values[i] = res;
+            count++;
+            // 所有状态都变成fulfilled时返回的MyPromise状态就变成fulfilled
+            if (count === list.length) resolve(values);
+          },
+          (err) => {
+            // 有一个被rejected时返回的MyPromise状态就变成rejected
+            reject(err);
+          }
+        );
+      }
+    });
+  }
 }
 function resolvePromise(promise2, x, resolve, reject) {
   // 循环引用报错
@@ -154,26 +181,4 @@ function resolvePromise(promise2, x, resolve, reject) {
      } else {
     resolve(x);
   }
-}
-
-static all (list) {
-  return new MyPromise((resolve, reject) => {
-    /**
-     * 返回值的集合
-     */
-    let values = []
-    let count = 0
-    for (let [i, p] of list.entries()) {
-      // 数组参数如果不是MyPromise实例，先调用MyPromise.resolve
-      this.resolve(p).then(res => {
-        values[i] = res
-        count++
-        // 所有状态都变成fulfilled时返回的MyPromise状态就变成fulfilled
-        if (count === list.length) resolve(values)
-      }, err => {
-        // 有一个被rejected时返回的MyPromise状态就变成rejected
-        reject(err)
-      })
-    }
-  })
 }
